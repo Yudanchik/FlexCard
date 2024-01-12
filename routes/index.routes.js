b67796fbfd062a0ff355f4c8cde6879534547a94
Nikita <nik.aksenovv@gmail.com>
2024-01-12 08:28:19 +0300

    changes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const mainRouter = require('./MainRoutes');
+const mainRouter = require('./Main.routes');
 
 
 router.use('/', mainRouter)
